7fff2ca05a453e9f7a396bb8ac5beb1980cb5e6c
Maria79 <mariajalbisu@gmail.com>
2023-04-28 11:51:58 +0100

    2804

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import getCurrentUser from './actions/getCurrentUser';
-import getListings, { IListingsParams } from './actions/getListings';
-import ClientOnly from './components/ClientOnly';
 import Container from './components/Container';
-import EmptyState from './components/EmptyState';
 import ListingCard from './components/listings/ListingCard';
+import EmptyState from './components/EmptyState';
+
+import getListings, { IListingsParams } from './actions/getListings';
+
+import getCurrentUser from './actions/getCurrentUser';
+import ClientOnly from './components/ClientOnly';
 
 interface HomeProps {
 	searchParams: IListingsParams;
@@ -25,7 +27,7 @@ const Home = async ({ searchParams }: HomeProps) => {
 		<ClientOnly>
 			<Container>
 				<div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-					{listings.map((listing) => {
+					{listings.map((listing: any) => {
 						return (
 							<ListingCard
 								key={listing.id}
