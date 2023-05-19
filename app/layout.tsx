b6f76353de5c6c9089a761ed1bdcb10fafab1275
Maria79 <mariajalbisu@gmail.com>
2023-05-19 22:12:34 +0100

    1905

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from './components/navbar/Navbar';
 import LoginModal from './components/modals/LoginModal';
 import RegisterModal from './components/modals/RegisterModal';
 import RentModal from './components/modals/RentModal';
-import SearchModal from './components/modals/searchModal';
+import SearchModal from './components/modals/SearchModal';
 
 import ToasterProvider from './providers/ToasterProvider';
 
